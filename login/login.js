a6c681fbfaad4c682eb58fe7a05116976b2b5490
Axzard <musyaitirjr@gmail.com>
2024-11-17 22:45:43 +0700

    first commit

---
+++ login/login.js
@@ -0,0 +1,26 @@
+const togglePassword = document.querySelector('.toggle-password');
+const passwordInput = document.getElementById('password');
+const showIcon = document.querySelector('.show-icon');
+const hideIcon = document.querySelector('.hide-icon');
+
+togglePassword.addEventListener('click', () => {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        showIcon.style.display = 'none';
+        hideIcon.style.display = 'inline';
+    } else {
+        passwordInput.type = 'password';
+        showIcon.style.display = 'inline';
+        hideIcon.style.display = 'none';
+    }
+});
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username && password) {
+        window.location.href = "home.html";
+    } else {
+        alert("Silakan masukkan username dan password yang valid");
+    }
+});
